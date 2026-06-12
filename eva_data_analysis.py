44120079299a550a7c922acfc7ee0c0c740e842b
Marine Balcou <balcoumarine@gmail.com>
2026-06-12 11:19:19 +0100

    Give meaningful variable names

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,47 +4,47 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('./eva-data.json', 'r', encoding="ascii")
-data_t = open('./eva-data.csv','w', encoding="utf-8")
-g_file = './cumulative_eva_graph.png'
+input_file = open('./eva-data.json', 'r', encoding="ascii")
+output_file = open('./eva-data.csv','w', encoding="utf-8")
+graph_file = './cumulative_eva_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 data=[]
 
 for i in range(375):
-    line=data_f.readline()
+    line=input_file.readline()
     print(line)
     data.append(json.loads(line[1:-1]))
 #data.pop(0)
 ## Comment out this bit if you don't want the spreadsheet
 
-w=csv.writer(data_t)
+processed_data=csv.writer(output_file)
 
 time = []
 date =[]
 
-j=0
+line=0
 for i in data:
-    print(data[j])
+    print(data[line])
     # and this bit
-    w.writerow(data[j].values())
-    if 'duration' in data[j].keys():
-        tt=data[j]['duration']
-        if tt == '':
+    processed_data.writerow(data[line].values())
+    if 'duration' in data[line].keys():
+        duration_str=data[line]['duration']
+        if duration_str == '':
             pass
         else:
-            t=dt.datetime.strptime(tt,'%H:%M')
-            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
-            print(t,ttt)
-            time.append(ttt)
-            if 'date' in data[j].keys():
-                date.append(dt.datetime.strptime(data[j]['date'][0:10], '%Y-%m-%d'))
-                #date.append(data[j]['date'][0:10])
+            duration_dt=dt.datetime.strptime(duration_str,'%H:%M')
+            duration_hours = dt.timedelta(hours=duration_dt.hour, minutes=duration_dt.minute, seconds=duration_dt.second).total_seconds()/(60*60)
+            print(duration_dt,duration_hours)
+            time.append(duration_hours)
+            if 'date' in data[line].keys():
+                date.append(dt.datetime.strptime(data[line]['date'][0:10], '%Y-%m-%d'))
+                #date.append(data[line]['date'][0:10])
 
             else:
                 time.pop(0)
-    j+=1
+    line+=1
 
 t=[0]
 for i in time:
@@ -57,5 +57,5 @@ plt.plot(date,t[1:], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
-plt.savefig(g_file)
+plt.savefig(graph_file)
 plt.show()
